4bfa0c9a8065b5ab3df1043de602d5b83d528128
Pavel <pavbobek@seznam.cz>
2023-02-12 10:47:26 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using tetofo.EventBus.Impl;
+using tetofo.Stem;
+using tetofo.Stem.Impl;
+
+namespace tetofo;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        using IStem eventPayloadLoggerStem = new EventPayloadLoggerStem();
+        using IStem consoleReaderStem = new ConsoleReaderStem();
+        consoleReaderStem.Subscribe(eventPayloadLoggerStem);
+        consoleReaderStem.Event(new StartReadingConsoleEvent());
+        while(true) {}
+    }
+}
